10718a33bba1d7d1cc9fccd74bc5ff678ada05d1
Corey Lafferty <clafferty@forumone.com>
2015-03-11 15:13:17 -0400

    created new panels layouts and removed default ones

--- public/sites/all/themes/gesso/panels/layouts/sidebar_right/sidebar-right.tpl.php
+++ public/sites/all/themes/gesso/panels/layouts/sidebar_right/sidebar-right.tpl.php
@@ -5,21 +5,23 @@
   $classes = "$preface $main $sidebar";
 ?>
 
-<div<?php if (!empty($css_id)): ?> id="<?php print $css_id; ?>"<?php endif; ?> class="layout-panels-sidebar <?php print $classes; ?>">
+<div<?php if (!empty($css_id)): ?> id="<?php print $css_id; ?>"<?php endif; ?> class="l-panels-sidebar-right <?php print $classes; ?>">
   <?php if (!empty($content['preface'])): ?>
-    <div class="layout-preface">
+    <div class="l-preface">
       <?php print $content['preface']; ?>
     </div>
   <?php endif; ?>
   <?php if (!empty($content['main'])): ?>
-    <div class="layout-main">
+    <div class="l-main">
       <?php print $content['main']; ?>
     </div>
   <?php endif; ?>
   <?php if (!empty($content['sidebar'])): ?>
-    <aside class="layout-sidebar">
+    <aside class="l-sidebar">
       <?php print $content['sidebar']; ?>
     </aside>
   <?php endif; ?>
 </div>
 
+ 
+ 
