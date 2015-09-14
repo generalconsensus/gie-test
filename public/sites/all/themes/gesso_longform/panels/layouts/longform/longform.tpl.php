bd1a5ec3cdb1475aa67c6838ea4d990adfe3af1c
Derek Moon <dmoon@forumone.com>
2015-09-14 15:45:43 -0700

    Changes with Corey

--- public/sites/all/themes/gesso_longform/panels/layouts/longform/longform.tpl.php
+++ public/sites/all/themes/gesso_longform/panels/layouts/longform/longform.tpl.php
@@ -1,11 +1,10 @@
 <?php
   $preface = (empty($content['preface']) ? 'preface-empty' : 'preface');
   $main = (empty($content['main']) ? 'main-empty' : 'main');
-  $sidebar = (empty($content['sidebar']) ? 'sidebar-empty' : 'sidebar');
-  $classes = "$preface $main $sidebar";
+  $classes = "$preface $main";
 ?>
 
-<div<?php if (!empty($css_id)): ?> id="<?php print $css_id; ?>"<?php endif; ?> class="layout-panels-sidebar <?php print $classes; ?>">
+<div<?php if (!empty($css_id)): ?> id="<?php print $css_id; ?>"<?php endif; ?> class="layout-panels-longform <?php print $classes; ?>">
   <?php if (!empty($content['preface'])): ?>
     <div class="layout-preface">
       <?php print $content['preface']; ?>
@@ -15,11 +14,6 @@
     <div class="layout-main">
       <?php print $content['main']; ?>
     </div>
-  <?php endif; ?>
-  <?php if (!empty($content['sidebar'])): ?>
-    <aside class="layout-sidebar">
-      <?php print $content['sidebar']; ?>
-    </aside>
   <?php endif; ?>
 </div>
 
