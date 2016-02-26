a0e1ef22347446823037a6b5632edac797c61012
mcburge <matt.burge@gmail.com>
2016-02-25 19:58:19 -0500

    Created Microsite Highlight, revised Microsite Header to use new display template

--- public/sites/all/themes/gesso/ds_layouts/microsite/microsite-header.tpl.php
+++ public/sites/all/themes/gesso/ds_layouts/microsite/microsite-header.tpl.php
@@ -1,19 +1,21 @@
 <?php
 /**
  * @file
- * microsite-text_callout.tpl.php
+ * microsite-header.tpl.php
  */
 ?>
 
 <?php
 
   //$field_para_5050_orientation = field_get_items('paragraphs_item', $variables['paragraphs_item'], 'field_para_text_orientation');
-  $background = $content['field_background'][0]['#markup'];
+  //get value from the header options
+  $options = $content['field_para_header_options'][0]['#markup'];
   //check to see if there is an image value set
-  $imagefield = $content['field_para_callout_image']['#items'][0]['uri'];
-  $image = file_create_url($content['field_para_callout_image']['#items'][0]['uri']);
+  $imagefield = $content['field_para_header_image']['#items'][0]['uri'];
+  //get image url 
+  $image = file_create_url($content['field_para_header_image']['#items'][0]['uri']);
   //set classes, including background value
-  $classes = "microsite-break " . ' ' . $background;
+  $classes = "microsite-header" . ' ' . $options;
 
   //var_dump($content['field_background'][0]['#markup']);
 
@@ -24,8 +26,8 @@
     $styles = $imagefield;
   };
 
-  hide($content['field_background']);
-  hide($content['field_para_callout_image'])
+  hide($content['field_para_header_options']);
+  hide($content['field_para_header_image']);
 ?>
 
 <div class="<?php print $classes ?>" <?php print $styles ?>>
