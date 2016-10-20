f172bbf4bcd34ddcc995b714350b578b27d34630
Daniel McDermott <dmcdermott@forumone.com>
2016-10-20 15:16:57 -0700

    GIEDFL-2 #comment Disable title

--- public/sites/all/themes/gesso_longform/ds_layouts/longform/long-form-background-video.tpl.php
+++ public/sites/all/themes/gesso_longform/ds_layouts/longform/long-form-background-video.tpl.php
@@ -5,11 +5,6 @@
  */
 ?>
 
-<?php if (!empty($content['field_background_video_text'][0]['#markup'])): ?>
-  <div class="section__video-text">
-    <h1><?php echo $content['field_background_video_text'][0]['#markup']; ?></h1>
-  </div>
-<?php endif; ?>
 
 <?php if (!empty($content['field_background_video_video']['#object']) && is_object(
     $content['field_background_video_video']['#object']
@@ -17,16 +12,16 @@
     $content['field_background_video_video']['#object']->field_background_video_video['und'][0]['entity']
   )
 ):
+  // Hide title
+  hide($content['field_background_video_text']);
+
   $url = $content['field_background_video_video']['#object']->field_background_video_video['und'][0]['entity']->field_background_video_upload['und'][0]['uri'];
   $url = (file_create_url($url)); ?>
 
   <div id="long_form_background_video_wrapper">
-
     <div class="section__video-wrapper">
-      <video class="section__video" width="1920" height="10800" autoplay=""
-             muted="" loop="">
-        <source class="video" src="<?php print $url ?>"
-                type="video/mp4">
+      <video class="section__video" autoplay="" muted="" loop="">
+        <source class="video" src="<?php print $url ?>" type="video/mp4">
       </video>
     </div>
 
