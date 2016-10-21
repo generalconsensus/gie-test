bc540e9b05a5fb663a88aa94249fc109a7fbe9f2
Corey Lafferty <coreylafferty@gmail.com>
2016-10-20 21:18:06 -0400

    updated markup in enhanced text

--- public/sites/all/themes/gesso_longform/ds_layouts/longform/long-form-enhanced-text.tpl.php
+++ public/sites/all/themes/gesso_longform/ds_layouts/longform/long-form-enhanced-text.tpl.php
@@ -8,7 +8,7 @@
 <?php
   // Set our classes
   $orientation = $field_longform_orientation[0]['value'] . '-justified';
-  $classes = implode(' ', array_filter(['long-form-enhanced-text', $orientation]));
+  $classes = implode(' ', array_filter(['enhanced-text component', $orientation]));
 
   // Get image uri and title, and create HTML styling
   $image = (!empty($field_longform_image)) ? $field_longform_image[0] : null;
@@ -23,5 +23,11 @@
 ?>
 
 <div class="<?php print $classes?>" <?php print $styles ?>>
-  <?php print render($content); ?>
+  <div class="enhanced-text__wrapper">
+    <div class="enhanced-text__inner">
+      <div class="enhanced-text__text">
+        <?php print render($content); ?>
+      </div>
+    </div>
+  </div>
 </div>
