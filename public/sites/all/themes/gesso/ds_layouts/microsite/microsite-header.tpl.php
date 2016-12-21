246145a9cf496cc45908fb17d6e5af4b05bcd529
Michael Drayer <mdrayer@forumone.com>
2016-12-21 12:36:38 -0500

    GIEYP-68 Add "Size" field to Microsite Header. Add logic to add a '--small' class to the microsite header template.

--- public/sites/all/themes/gesso/ds_layouts/microsite/microsite-header.tpl.php
+++ public/sites/all/themes/gesso/ds_layouts/microsite/microsite-header.tpl.php
@@ -7,16 +7,19 @@
 
 <?php
 
-  //$field_para_5050_orientation = field_get_items('paragraphs_item', $variables['paragraphs_item'], 'field_para_text_orientation');
+  // set the class for this paragraph type
+  $header_class = 'microsite-header';
   //get value from the header options
   $options = (!empty($content['field_para_header_options'])) ? $content['field_para_header_options'][0]['#markup'] : '';
+  // get header size value and set the size class
+  $size = (!empty($content['field_para_header_size'])) ? $content['field_para_header_size'][0]['#markup'] : '';
+  $size_class = ($size === 'small') ? $header_class . '--small' : '';
   //check to see if there is an image value set
   $imagefield = (!empty($content['field_para_header_image'])) ? $content['field_para_header_image']['#items'][0]['uri'] : null;
   //get image url
   $image = ($imagefield) ? file_create_url($content['field_para_header_image']['#items'][0]['uri']) : null;
   //set classes, including background value
-  $classes = implode(' ', array_filter(['microsite-header', $options]));
-  //var_dump($content['field_background'][0]['#markup']);
+  $classes = implode(' ', array_filter([$header_class, $options, $size_class]));
 
   //if image set, then set style, if not do nothing.
   if (isset($imagefield)) {
@@ -25,8 +28,10 @@
     $styles = $imagefield;
   };
 
+  // hide the fields we do not want displayed
   hide($content['field_para_header_options']);
   hide($content['field_para_header_image']);
+  hide($content['field_para_header_size']);
 ?>
 
 <div class="<?php print $classes ?>" <?php print $styles ?> <?php if (!(empty($content['field_para_header_image']['#items'][0]['title']))) { print ('title="' . $content['field_para_header_image']['#items'][0]['title'] . '"'); } ?>>
