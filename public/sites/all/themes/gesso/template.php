4f882578eadc5d8c16703c7fd2a6b7b80de5deb0
Daniel McDermott <dmcdermott@forumone.com>
2017-01-21 23:27:15 -0800

    GIE-952 #comment Added custom form, callback, custom setup for making sure that the translation menu only shows up for certain domains

--- public/sites/all/themes/gesso/template.php
+++ public/sites/all/themes/gesso/template.php
@@ -134,7 +134,9 @@ function gesso_comment_post_forbidden($variables) {
 
 function gesso_menu_tree__user_menu($variables) {
   $menu = '<ul class="nav">' ;
-  if (user_has_role(10)) {
+  global $_domain;
+
+  if (user_has_role(10) && in_array($_domain['domain_id'], variable_get('gie_translation_menu_domains'))) {
     $menu .= '<li class="nav__item"><a href="#" data-remodal-target="modal-language-switcher" class="nav__link nav__link--language">Language</a></li>';
   }
   return  $menu . $variables['tree'] . '</ul>';
